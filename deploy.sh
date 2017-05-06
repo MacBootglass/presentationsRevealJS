238629bbc571fc11dbeba97db24f39dd176b8fc7
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-05-06 16:22:49 +0200

    UPDATE - deploy travis

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 git config user.name "MacBootglass"
 git config user.email "thibault.theologien@insa-rouen.fr"
-git add .
+git add -f public/*
+git add -f node_modules/*
 git commit -m "Deployed with Travis and from presentationsRevealJS"
 git push -fq --set-upstream "https://${GH_TOKEN}@${GH_REF}" master
