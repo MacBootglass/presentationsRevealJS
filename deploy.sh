68efaca05ce898d276e86735679f6be9ad17f0ca
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-05-07 13:53:53 +0200

    UPDATE - deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,10 @@ cp public/index.html dist
 cp -rf node_modules/jquery dist/ressources/jquery
 cp -rf node_modules/reveal.js dist/ressources/revealjs
 cd dist
-git init
-git config user.name "MacBootglass"
-git config user.email "thibault.theologien@insa-rouen.fr"
-git add .
-git commit -m "Deployed with Travis and from presentationsRevealJS"
-git push -fq --set-upstream "https://${GH_TOKEN}@${GH_REF}" master
+# git init
+# git config user.name "MacBootglass"
+# git config user.email "thibault.theologien@insa-rouen.fr"
+# git add .
+# git commit -m "Deployed with Travis and from presentationsRevealJS"
+# git push -fq --set-upstream "https://${GH_TOKEN}@${GH_REF}" master
+find mydir -type f -exec curl --user macbootglass-presentations\\ttheologien:${password} --ftp-create-dirs -T {} ftp://waws-prod-am2-027.ftp.azurewebsites.windows.net/site/wwwroot/{}
