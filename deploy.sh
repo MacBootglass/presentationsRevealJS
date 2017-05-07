b8ed1d8eebc63ceffaa19f43e7966a7b6e30cefc
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-05-07 14:21:15 +0200

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,10 @@ mkdir dist/jquery
 cp -rf public/presentations dist/presentations
 cp -rf public/ressources dist/ressources
 cp public/index.html dist
-cp -rf node_modules/jquery dist/ressources/jquery
-cp -rf node_modules/reveal.js dist/ressources/revealjs
+cp -rf node_modules/jquery/dist dist/ressources/jquery/
+cp -rf node_modules/reveal.js/js dist/ressources/revealjs/
+cp -rf node_modules/reveal.js/css dist/ressources/revealjs/
+cp -rf node_modules/reveal.js/plugin dist/ressources/revealjs/
 cd dist
 # git init
 # git config user.name "MacBootglass"
